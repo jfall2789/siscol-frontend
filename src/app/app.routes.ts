945618935cbcd3344730c7c6fdf628e56f66db03
Juan Francisco Amézquita <jfall2789@gmail.com>
2026-07-05 01:52:29 -0500

    Se agrego la tan esperada pantalla de incidencias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,39 @@
 import { Routes } from '@angular/router';
 
 import { Login } from './features/auth/login/login';
+
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+
 import { Home } from './features/dashboard/home/home';
+
 import { IncidentList } from './features/incident/incident-list/incident-list';
 
 export const routes: Routes = [
 
-  // Login
   {
     path: '',
     component: Login
   },
 
-  // Dashboard
   {
     path: 'dashboard',
     component: DashboardLayout,
     children: [
 
-      // Home
       {
         path: '',
         component: Home
       },
 
-      // Gestión de Incidencias
       {
         path: 'incidents',
         component: IncidentList
       }
 
     ]
+
   },
 
-  // Redirección para rutas inexistentes
   {
     path: '**',
     redirectTo: ''
